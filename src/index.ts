c3dc09619d67dcd487eec42bffb26c57530c2b17
Mosab Samara (ASAL Technologies Ltd) <v-mossam@microsoft.com>
2020-04-12 13:57:14 +0300

    Implement two more APIs and their related interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import * as axios from 'axios';
-import cron from 'node-cron';
 import express from 'express';
+import cron from 'node-cron';
+import { getDetailsHttpResponse, getGovernoratesSummaryHttpResponse, getSummaryHttpResponse } from './APIs';
 import * as constants from './constants';
-import { ISummaryResponse } from './interfaces';
-import { httpGet, isValidJSONResponse } from './utils';
-
-// method to call corona.ps APIs
-async function getSummaryHttpResponse(): Promise<axios.AxiosResponse<ISummaryResponse>> {
-  return httpGet(constants.SUMMARY_API_URL);
-};
+import { IDetailsResponse, IGovernoratesSummaryResponse, ISummaryResponse } from './interfaces';
+import { isValidJSONResponse } from './utils';
 
 /**
  * Check whether to Update Firebase Data or not.
@@ -21,13 +16,15 @@ function shouldPerformUpdate(
   prevSummaryJSON: ISummaryResponse,
   currentSummaryJSON: ISummaryResponse
 ): boolean {
+
   const prevLastUpdatedAt = prevSummaryJSON.data ? Date.parse(prevSummaryJSON.data.LastUpdated) : -1;
   const currentLastUpdatedAt = Date.parse(currentSummaryJSON.data.LastUpdated);
-
   return Math.abs(prevLastUpdatedAt - currentLastUpdatedAt) !== 0;
 };
 
-let summaryJSON = ({} as ISummaryResponse); // variable to hold API results
+let summaryJSON = ({} as ISummaryResponse); // variable to hold summary API results
+let detailsJSON = ({} as IDetailsResponse); // variable to hold details API results
+let governoratesSummaryJSON = ({} as IGovernoratesSummaryResponse); // variable to hold governorate summary API results
 const app = express()
 
 // schedule a cron job to call corona.ps API every 30 minutes
@@ -36,11 +33,21 @@ cron.schedule(constants.CRON_JOB_NOTATION_EVERY_30_MIN, async () => {
   if (isValidJSONResponse(incomingSummaryHttpResponse)) {
     const incomingSummaryJSON = incomingSummaryHttpResponse.data as ISummaryResponse;
     console.log("Summary as JSON:", incomingSummaryJSON);
-    console.log("Previous LastUpdated: ", ((summaryJSON.data)||{}).LastUpdated || undefined);
+    console.log("Previous LastUpdated: ", ((summaryJSON.data) || {}).LastUpdated || undefined);
     console.log("Current LastUpdated: ", incomingSummaryJSON.data.LastUpdated);
     // compare the two dates, to check if the server data was updated
     if (shouldPerformUpdate(summaryJSON, incomingSummaryJSON)) {
       summaryJSON = JSON.parse(JSON.stringify(incomingSummaryJSON));
+      const incomingDetailsHttpResponse = await getDetailsHttpResponse();
+      if (isValidJSONResponse(incomingDetailsHttpResponse)) {
+        detailsJSON = JSON.parse(JSON.stringify(incomingDetailsHttpResponse.data as IDetailsResponse));
+        console.log('Details as JSON: ', detailsJSON);
+      }
+      const incomingGovernorateSummaryHttpResponse = await getGovernoratesSummaryHttpResponse();
+      if (isValidJSONResponse(incomingGovernorateSummaryHttpResponse)) {
+        governoratesSummaryJSON = JSON.parse(JSON.stringify(incomingGovernorateSummaryHttpResponse.data as IGovernoratesSummaryResponse));
+        console.log('Governorates summary as JSON: ', governoratesSummaryJSON);
+      }
       console.log("Sending to firebase"); // initial message until firebase is done
     }
   }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -12,3 +12,36 @@ export interface ISummaryResponse {
     readonly DetailedMap: string;
   },
 };
+
+export interface ICase {
+  case_number: string,
+  case_age: string,
+  case_gender: string,
+  case_location: string,
+  case_diagnose_date: string,
+  case_source_of_infection: string,
+  case_condition: string,
+  case_quarantine: string
+};
+
+export interface IDetailsResponse {
+  data: {
+    cases: ICase[]
+  },
+};
+
+export interface IGovernorate {
+  Name: string,
+  Cases: string,
+  CentralQuarantine: string,
+  HomeQuarantine: string,
+  Recovered: number,
+  Deaths: number
+};
+
+export interface IGovernoratesSummaryResponse {
+  data: {
+    Governorates: IGovernorate[]
+  },
+};
+
